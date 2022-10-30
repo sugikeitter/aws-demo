1b9c35b9d7212ea488a45eca83dc05b1918b01c4
sugikeitter <sugikei@example.com>
2022-10-30 22:55:14 +0900

    Add cdk multi vpc stack

---
+++ cdk-sharedvpc-tgw-vpnvpc/bin/cdk-sharedvpc-with-albecs-and-vpnvpc.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CdkSharedvpcWithAlbecsAndVpnvpcStack } from '../lib/cdk-sharedvpc-with-albecs-and-vpnvpc-stack';
+
+const app = new cdk.App();
+new CdkSharedvpcWithAlbecsAndVpnvpcStack(app, 'CdkSharedvpcWithAlbecsAndVpnvpcStack', {
+  env: { account: process.env.AWS_ACCOUNT_ID, region: process.env.AWS_REGION }
+});
---
+++ cdk-sharedvpc-tgw-vpnvpc/test/cdk-sharedvpc-with-albecs-and-vpnvpc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSharedvpcWithAlbecsAndVpnvpc from '../lib/cdk-sharedvpc-with-albecs-and-vpnvpc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-sharedvpc-with-albecs-and-vpnvpc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSharedvpcWithAlbecsAndVpnvpc.CdkSharedvpcWithAlbecsAndVpnvpcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
