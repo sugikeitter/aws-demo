dcca7bc60d652be242377687fe57030a028c9a6c
sugikeitter <s.l.n.f.13_kei.s@me.com>
2022-11-13 16:44:09 +0900

    Add EC2+ASG to ALB w/ SG considering NWFW

--- cdk-sharedvpc-tgw-vpnvpc/lib/shared-vpc-alb.ts
+++ cdk-sharedvpc-tgw-vpnvpc/lib/shared-vpc-alb.ts
@@ -1,8 +1,10 @@
 import {
+  aws_autoscaling as asg,
   aws_ec2 as ec2,
   aws_elasticloadbalancingv2 as elb,
   aws_ecs as ecs,
   aws_iam as iam,
+  Duration,
 } from 'aws-cdk-lib'
 import { Construct } from 'constructs';
 
@@ -12,6 +14,8 @@ export interface SharedVpcAlbProps {
 export class SharedVpcAlb extends Construct {
   constructor(scope: Construct, id: string, props: SharedVpcAlbProps) {
     super(scope, id);
+
+    /* ALB, Listener, TargetGroup */
     const albSg = new ec2.SecurityGroup(this, 'AlbSg', {
       vpc: props.vpc,
     }); // ルールはALBのリスナーを作成すると自動生成されるのでここでは設定しない
@@ -26,35 +30,41 @@ export class SharedVpcAlb extends Construct {
       securityGroup: albSg
     });
 
+    // ListenerはデフォルトでECS(ローリングデプロイ)、クエリパラメーターでEC2+ASGやECS(Blue/Green)のターゲットへ振り分け
     const albListener = alb.addListener('HttpListener', {
       port: 80,
       protocol: elb.ApplicationProtocol.HTTP,
       open: true,
     });
 
+    // ECS(ローリングデプロイ)用
     const targetGroup = albListener.addTargets('ecs', {
-      targetGroupName: "ecs",
+      targetGroupName: "demoEcs",
       port: 80,
       targets: [],
       healthCheck: {
-        path: "/health"
+        path: "/health",
+        healthyThresholdCount: 2,
+        unhealthyThresholdCount: 3
       }
     });
-    // `albListener.addTargets` 使わず `new elb.CfnTargetGroup` 使った方が良いかも？
+    // `albListener.addTargets`の場合、targetTypeが指定できず"instance"になるが、ECSの場合は"ip"にしたい
+    // もしかしたら`albListener.addTargets` 使わず `new elb.CfnTargetGroup` 使った方が良いかも？
     const cfnTargetGroup = targetGroup.node.children.find(
-       // TODO id が 'Resource' でTargetGroupが取得できる理由が謎だが、console.error(targetGroup.node.children)で中を見たところ、このnodeしか存在しなかったのでひとまず
+       // `console.error(targetGroup.node.children)`で中を見たところ、id が 'Resource'のnodeしか存在しなかったのでひとまず
       (child) => child.node.id == 'Resource'
     ) as elb.CfnTargetGroup ;
     cfnTargetGroup.targetType = 'ip';
-    // console.error(targetGroup.node.children);
 
-    // クエリパラメーター(tg=ecs-bg や tg=ec2-asg などでの)でターゲットグループ振り分けルールの設定
-    albListener.addTargets('ec2-asg', {
-      targetGroupName: "ec2-asg",
+    // クエリパラメーター(tg=ec2-asg)
+    const targetGroupEc2Asg = albListener.addTargets('ec2-asg', {
+      targetGroupName: "demoEc2-asg",
       port: 80,
       targets: [],
       healthCheck: {
-        path: "/health"
+        path: "/health",
+        healthyThresholdCount: 2,
+        unhealthyThresholdCount: 3
       },
       conditions: [
         elb.ListenerCondition.queryStrings([
@@ -64,12 +74,15 @@ export class SharedVpcAlb extends Construct {
       priority: 1
     });
 
+    // クエリパラメーター(tg=ecs-bg)
     const targetGroupEcsBg1 = albListener.addTargets('ecs-bg-1', {
-      targetGroupName: "ecs-bg-1",
+      targetGroupName: "demoEcs-bg-1",
       port: 80,
       targets: [],
       healthCheck: {
-        path: "/health"
+        path: "/health",
+        healthyThresholdCount: 2,
+        unhealthyThresholdCount: 3
       },
       conditions: [
         elb.ListenerCondition.queryStrings([
@@ -79,11 +92,12 @@ export class SharedVpcAlb extends Construct {
       priority: 2
     });
     const cfnTargetGroupEcsBg1 = targetGroupEcsBg1.node.children.find(
-      // TODO id が 'Resource' でTargetGroupが取得できる理由が謎だが、console.error(targetGroup.node.children)で中を見たところ、このnodeしか存在しなかったのでひとまず
+      // `console.error(targetGroup.node.children)`で中を見たところ、id が 'Resource'のnodeしか存在しなかったのでひとまず
       (child) => child.node.id == 'Resource'
     ) as elb.CfnTargetGroup ;
     cfnTargetGroupEcsBg1.targetType = 'ip';
 
+    // ECS(Blue/Green)はListenerが2つ必要
     const albListenerEcsBg = alb.addListener('HttpListenerEcsBg', {
       port: 9000,
       protocol: elb.ApplicationProtocol.HTTP,
@@ -91,11 +105,13 @@ export class SharedVpcAlb extends Construct {
     });
 
     const targetGroupEcsBg2 = albListenerEcsBg.addTargets('ecs-bg-2', {
-      targetGroupName: "ecs-bg-2",
+      targetGroupName: "demoEcs-bg-2",
       port: 80,
       targets: [],
       healthCheck: {
-        path: "/health"
+        path: "/health",
+        healthyThresholdCount: 2,
+        unhealthyThresholdCount: 3
       },
       // * ポート9000で受け付けるターゲットは1つしかないのでconditionsは不要
       // conditions: [
@@ -105,11 +121,64 @@ export class SharedVpcAlb extends Construct {
       // ]
     });
     const cfnTargetGroupEcsBg2 = targetGroupEcsBg2.node.children.find(
-      // TODO id が 'Resource' でTargetGroupが取得できる理由が謎だが、console.error(targetGroup.node.children)で中を見たところ、このnodeしか存在しなかったのでひとまず
+      // `console.error(targetGroup.node.children)`で中を見たところ、id が 'Resource'のnodeしか存在しなかったのでひとまず
       (child) => child.node.id == 'Resource'
     ) as elb.CfnTargetGroup ;
     cfnTargetGroupEcsBg2.targetType = 'ip';
 
+    /* EC2+ASG */
+    // ALBからのみアクセスを受けるEC2のSG
+    const ec2Sg = new ec2.SecurityGroup(this, 'Ec2FromPublicSubnet', {
+      vpc: props.vpc,
+    });
+    // TODO ALB→NWFW→EC2のサブネットを経由している場合、EC2のSGにALBのSGを指定してもヘルスチェックが到達しない
+    // ec2Sg.addIngressRule(albSg, ec2.Port.tcp(80));
+
+    // TODO とりあえずEC2のSGにはALBのサブネットのIP CIDR指定が必要(もしくはNWFWエンドポイントのENIにSG設定する)
+    //  SharedVPCのパブリックサブネットにALBを作成し、そのALB経由のIngressのトラフィックにNWFWはしない方が良いのか？
+    //  ALBはTGWでSharedVPCに接続できる別のVPCにパブリックサブネット(NATGWはなし)を用意して、そのVPCでALB→EC2を用意すべき？
+    props.vpc.selectSubnets({subnetGroupName: 'public'}).subnets.forEach(subnet => {
+      ec2Sg.addIngressRule(ec2.Peer.ipv4(subnet.ipv4CidrBlock), ec2.Port.tcp(80));
+    });
+
+    // 起動テンプレート
+    const userData = ec2.UserData.forLinux();
+    // 本来は必要なパッケージをインストールしたAMIを利用すべき
+    userData.addCommands(
+      "sudo -u ec2-user sh -c \"curl https://raw.githubusercontent.com/sugikeitter/golang__htmlServerOnAws/main/bin/go-http-linux > /home/ec2-user/httpServer\"",
+      "sudo -u ec2-user chmod 755 /home/ec2-user/httpServer",
+      "nohup sudo /home/ec2-user/httpServer 80 &"
+    );
+    const launchTemplate = new ec2.LaunchTemplate(this, 'AmznLinux2', {
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+      }),
+      userData: userData,
+      securityGroup: ec2Sg,
+      role: iam.Role.fromRoleName(this, 'SsmRoleForEc2', 'AmazonSSMRoleForInstancesQuickSetup'),
+    })
+
+    // AutoScaling Group(launcTemplateの変数を用意しなくてもASGのプロパティ指定でlaunchTemplateも作成してくれるが、分割した方が今後拡張しやすそうなので)
+    const ec2Asg = new asg.AutoScalingGroup(this, 'Asg', {
+      vpc: props.vpc,
+      autoScalingGroupName: "go-http",
+      desiredCapacity: 1,
+      healthCheck: asg.HealthCheck.elb({
+        grace: Duration.seconds(30)
+      }),
+      maxCapacity: 3,
+      minCapacity: 1,
+      launchTemplate: launchTemplate,
+      vpcSubnets: props.vpc.selectSubnets({subnetGroupName: 'privateA'})
+    });
+    ec2Asg.attachToApplicationTargetGroup(targetGroupEc2Asg);
+    ec2Asg.scaleOnRequestCount('300reqPerMinutes', {
+      targetRequestsPerMinute: 300
+    });
+
+    /* ECSクラスタとタスク実行ロールのみ用意 */
+    /* タスク定義はコンテナイメージpushトリガーにCI/CDしたいので、タスク定義に紐づくECSサービスもここでは作成しない */
     const ecsCluster = new ecs.Cluster(this, 'EcsCluster', {
       clusterName: "DemoFargate",
       vpc: props.vpc
