397092537079300d705b2680f57bf4f969638238
Keita Sugimoto <sugikeitter@yahoo.co.jp>
2022-11-27 11:15:45 +0000

    Add CloudWatch Evidently

---
+++ sam-apigw-lambda-canary/cwevidently/app.py
@@ -0,0 +1,39 @@
+import json
+
+import boto3
+
+evidently = boto3.client('evidently')
+
+def lambda_handler(event, context):
+  entityId = event.get('queryStringParameters', {}).get('entityId', 'No entityId')
+  # response examle
+  # {
+  #   "details": {},
+  #   "reason": "DEFAULT",
+  #   "value": "{ 
+  #         "boolValue": "false" 
+  #       }",
+  #   "valueType": "BOOLEAN",
+  #   "variation": "off",
+  # }
+  res = evidently.evaluate_feature(
+    entityId=entityId,
+    feature='boolFeature',
+    project='demo'
+  )
+
+  featureFlagVariation = res.get('variation', '')
+  msg = ''
+  if res.get('value', {}).get('boolValue', False):
+    msg = 'あなたはラッキー！特別に割引を提供します😻'
+
+  return {
+      'statusCode': 200,
+      'body': json.dumps({
+        'entityId': entityId,
+        'msg': msg,
+        'featureFlagVariation': featureFlagVariation
+      },
+      ensure_ascii=False,
+      indent=2),
+  }
